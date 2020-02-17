c76d283a3bd8d98afb7ab692cf45eecbb820eec7
“L-shurunlong <L-shurunlong@qianxin.com>
2020-02-17 19:20:46 +0800

    第一天学习python笔记 python安装 配置环境, 变量,数据类型

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pythonstudy
+
+边看《锦衣之下》,边学习的python笔记...
+
+![](assets/1581938361572.png)
+
---
+++ day01/04-数据类型.py
@@ -0,0 +1,19 @@
+intTest=1
+print(intTest)
+print(type(intTest))
+stringTest="hello world"
+print(stringTest)
+print(type(stringTest))
+c = True
+print(type(c)) # <class 'bool'> -- 布尔型
+e = [10, 20, 30]
+print(type(e)) # <class 'list'> -- 列表
+f = (10, 20, 30)
+print(type(f)) # <class 'tuple'> -- 元组
+h = {10, 20, 30}
+print(type(h)) # <class 'set'> -- 集合
+g = {'name': 'TOM', 'age': 20}
+print(type(g)) # <class 'dict'> -- 字典
+
+
+
